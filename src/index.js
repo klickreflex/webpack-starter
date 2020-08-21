664edd8c64943df6c2cfff50f099d694f1df3aca
Daniel Wentsch <hello@wentsch.me>
2020-08-21 10:19:57 +0200

    Remove unused dependencies

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import _ from 'lodash';
+// Styles
 import './styles/style.pcss';
-import 'lazysizes';
+
+// Templates & Components
 import './templates/index.njk';
 import './templates/components/card.njk';
 
-function component() {
-    const element = document.createElement('div');
-
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    return element;
-
-}
-
-document.body.appendChild(component());
+// Vendor dependencies
+import 'lazysizes'
