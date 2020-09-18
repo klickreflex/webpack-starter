ae2f678f25ac02c862df76949a758ed5ef3a0127
Daniel Wentsch <hello@wentsch.me>
2020-09-18 11:33:10 +0200

    Add PostCSS plugins for better CSS DX

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 module.exports = {
+    // Use postcss-scss parser to support inline comments
+    parser: 'postcss-scss',
+
     plugins: [
       require('postcss-import'),
       require('tailwindcss'),
+      require('postcss-nested'),
       require('autoprefixer'),
     ]
   }
